013715078c6392dbedc578e6496a2d91dad0bfb2
Jake Adams <jdadams@utah.gov>
2020-10-05 08:31:49 -0600

    feat: better documentation

--- boost/config.py
+++ boost/config.py
@@ -7,11 +7,19 @@ A module that contains the static names and places of feature classes
 '''
 
 feature_classes = {
-    'bb_service': 'BB_Service',
-    'address_points': 'AddressPoints',
-    'counties': 'Counties',
-    'municip': 'Municipalities',
-    'unincorp': 'UnIncorpAreas2010_Approx',
+    #: Input dataset names, set these (must all exist in the workspace gdb)
+    #: Current Broadband Service layer
+    'bb_service': 'Utilities_BroadbandService_20200930',
+    #: Current Address Points layer
+    'address_points': 'AddressPoints_20200923',
+    #: County Boundaries
+    'counties': 'county_boundaries',
+    #: Municipal Boundaries
+    'municip': 'municipal_boundaries',
+    #: Approximate unincorporated areas
+    'unincorp': 'unincorporated_boundaries',
+
+    #: Output dataset names
     'address_service_final': 'Address_Service_Final',
     'msba': 'MSBA',
     'address_count_area': 'AddressCount_AreaName',
